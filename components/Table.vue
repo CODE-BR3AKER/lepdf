254952b473555067c6832246f44bd58d90139f18
CODE-BR3AKER <zarktizakaria@gmail.com>
2022-10-03 01:58:22 -0400

    added ap1 resources & made table modulaire & removed borders for mobile

---
+++ components/Table.vue
@@ -0,0 +1,61 @@
+<template>
+  <table class="text-title text-sm md:text-base">
+    <tr>
+      <th>Description</th>
+      <th class="hidden md:table-cell">Année</th>
+      <th>Action</th>
+    </tr>
+    <tr v-for="(file, id) in files" :key="id">
+      <td class="w-2/3 md:w-2/4">&#128196; {{ file.Filename }}</td>
+      <td class="hidden md:table-cell w-1/4 text-center">
+        {{ file.Year }}
+      </td>
+      <td class="w-1/3 md:w-1/4 text-center">
+        <a :href="file.DirectLink" class="text-links" :download="file.FileName"
+          ><i class="fas fa-file-download text-2xl hidden md:inline"></i
+          >Télécharger</a
+        >
+      </td>
+    </tr>
+  </table>
+</template>
+<script>
+export default {
+  props: ["files"],
+};
+</script>
+<style scoped>
+table {
+  width: 60vw;
+}
+th {
+  background: var(--main);
+  color: white;
+}
+table,
+th,
+td {
+  border: 2px solid var(--background);
+  border-collapse: collapse;
+  padding: 1em 2em;
+}
+tr:nth-child(even) {
+  background-color: var(--secondary);
+}
+@media only screen and (max-width: 768px) {
+  table {
+    width: 90vw;
+  }
+  th {
+    display: none;
+  }
+  tr:not(:last-child, :first-child) {
+    background: transparent !important;
+    border-bottom: 1px solid var(--secondary);
+  }
+  td {
+    border: 0;
+    padding: 1em 0;
+  }
+}
+</style>
--- pages/paths/ap1/_id.vue
+++ pages/paths/ap1/_id.vue
@@ -6,32 +6,17 @@
       {{ module }}
     </h1>
     <section class="pt-16 flex mx-auto justify-center items-center">
-      <table class="text-title text-sm md:text-base">
-        <tr>
-          <th>Description</th>
-          <th class="hidden md:table-cell">Année</th>
-          <th>Action</th>
-        </tr>
-        <tr v-for="(file, id) in files" :key="id">
-          <td class="w-2/3 md:w-2/4">&#128196; {{ file.Filename }}</td>
-          <td class="hidden md:table-cell w-1/4 text-center">2018</td>
-          <td class="w-1/3 md:w-1/4 text-center">
-            <a
-              :href="file.DirectLink"
-              class="text-links"
-              :download="file.FileName"
-              ><i class="fas fa-file-download text-2xl"></i>Télécharger</a
-            >
-          </td>
-        </tr>
-      </table>
+      <Table :files="files" />
     </section>
   </main>
 </template>
 <script>
+import { Year } from "faunadb";
 export default {
   async asyncData({ $content, params }) {
-    const path = await $content("paths", params.slug).fetch();
+    const path = await $content("paths", params.slug)
+      .sortBy(Year, "asc")
+      .fetch();
     return {
       path,
     };
@@ -55,27 +40,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-table {
-  width: 60vw;
-}
-@media only screen and (max-width: 768px) {
-  table {
-    width: 90vw;
-  }
-}
-th {
-  background: var(--main);
-  color: white;
-}
-table,
-th,
-td {
-  border: 2px solid var(--background);
-  border-collapse: collapse;
-  padding: 1em 2em;
-}
-tr:nth-child(even) {
-  background-color: var(--secondary);
-}
-</style>
